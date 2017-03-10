593b502791d58ecce2d02c592e94c4bf1fd2d71f
Dingyuan Wang <abcdoyle888@gmail.com>
2017-03-10 14:04:50 +0800

    add argparse

--- rehttpfs.py
+++ rehttpfs.py
@@ -7,6 +7,7 @@ import sys
 import stat
 import time
 import logging
+import argparse
 import calendar
 import urllib.parse
 from errno import EACCES, ENOENT, EIO
@@ -348,9 +349,14 @@ class rehttpfs(fuse.LoggingMixIn, fuse.Operations):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('usage: %s <url> <mountpoint>' % sys.argv[0])
-        sys.exit(1)
-
-    logging.basicConfig(level=logging.INFO)
-    fuseobj = fuse.FUSE(rehttpfs(sys.argv[1]), sys.argv[2], foreground=True)
+    parser = argparse.ArgumentParser(description="Mount HTML directory listings.")
+    parser.add_argument("-v", "--verbose", help="enable debug logging", action='store_true')
+    parser.add_argument("-d", "--daemon", help="run in background", action='store_true')
+    parser.add_argument("url", help="URL to mount")
+    parser.add_argument("mountpoint", help="filesystem mount point")
+    args = parser.parse_args()
+    logging.basicConfig(
+        format='%(levelname)s:%(name)s %(message)s',
+        level=logging.DEBUG if args.verbose else logging.INFO
+    )
+    fuseobj = fuse.FUSE(rehttpfs(args.url), args.mountpoint, foreground=(not args.daemon))
