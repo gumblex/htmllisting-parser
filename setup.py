1e1ac07b03922d7653f3b612699df40d82e101b0
Dingyuan Wang <gumblex@aosc.io>
2023-06-03 19:49:02 +0800

    version 0.6.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (3, 3):
 
 setup(
     name='htmllistparse',
-    version='0.6.0',
+    version='0.6.1',
     description='Python parser for Apache/nginx-style HTML directory listing.',
     long_description=open('README.rst', 'r').read(),
     author='Dingyuan Wang',
